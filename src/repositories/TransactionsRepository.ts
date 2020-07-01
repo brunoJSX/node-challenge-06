00777ca596d929a5d387595707fda3ca6ca8ec4f
Bruno Henrique <bruno@cbkdigital.com.br>
2020-06-30 22:30:09 -0400

    chore: To implement getBalance method and add router to list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,27 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+
+    const income = transactions
+      .filter(item => item.type === 'income')
+      .map(item => item.value)
+      .reduce((acumulador, currentItem) => acumulador + currentItem, 0);
+
+    const outcome = transactions
+      .filter(item => item.type === 'outcome')
+      .map(item => item.value)
+      .reduce((acumulador, currentItem) => acumulador + currentItem, 0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
+  }
 }
 
 export default TransactionsRepository;
